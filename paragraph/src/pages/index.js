15040071eb7bbffd1c7215cbb93166808f0b4195
Zachary Smith <22395357+Cutwell@users.noreply.github.com>
2023-07-02 22:13:15 +0100

    Moved SimpleMdeReactComponent to higher level

--- paragraph/src/pages/index.js
+++ paragraph/src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import loadable from '@loadable/component'
 
-const LoadableSimpleMdeReactComponent = loadable(() => import('./SimpleMdeReactComponent'))
+const LoadableSimpleMdeReactComponent = loadable(() => import('../SimpleMdeReactComponent'))
 
 const IndexPage = () => {
 	return (
