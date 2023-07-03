98a39300863a28df5063c878213902b82c537cf0
Zachary Smith <22395357+Cutwell@users.noreply.github.com>
2023-07-03 21:49:44 +0100

    Fixed static build
    * Moved SimpleMdeReact component

--- paragraph/src/pages/index.js
+++ paragraph/src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import loadable from '@loadable/component'
 
-const LoadableSimpleMdeReactComponent = loadable(() => import('../SimpleMdeReactComponent'))
+const LoadableSimpleMdeReactComponent = loadable(() => import('../components/SimpleMdeReactComponent'))
 
 const IndexPage = () => {
 	return (
